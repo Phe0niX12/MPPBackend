2ec836e2531441e34ddc13c52c479ed1ad292a44
916/SCHIOP ADRIAN-PETRU <adrianschiop2@gmail.com>
2024-05-24 22:54:38 +0300

    sper ultimu

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import routes from "./routes/routes.js";
 import { sequelize } from "./model/ModelSequalizer.js";
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 3000;
 
 
 
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 sequelize.sync({force:false})
 app.use("/api", routes);
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0",() => {
     console.log(`Server is running on port ${port}`);
     }
 );
